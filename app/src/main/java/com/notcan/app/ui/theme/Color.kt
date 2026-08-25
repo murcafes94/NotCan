67a2005f22c5a1875cf5325ad8eb54f94acd9292
murcafes94 <esaumuri94@gmail.com>
2026-08-25 15:19:54 -0500

    feat: add NotCan color palette

---
+++ app/src/main/java/com/notcan/app/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.notcan.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val NotCanBlack = Color(0xFF0D0F12)
+val NotCanGraphite = Color(0xFF171A1F)
+val NotCanSurface = Color(0xFF20242B)
+val NotCanBlue = Color(0xFF2457D6)
+val NotCanBlueSoft = Color(0xFF4B76E8)
+val NotCanRed = Color(0xFFD22F3E)
+val NotCanOffWhite = Color(0xFFF3F0E8)
+val NotCanGray = Color(0xFF8C939E)
+val NotCanBrown = Color(0xFF6E513B)
+val NotCanGreen = Color(0xFF3A9B67)
