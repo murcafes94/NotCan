a715b813b38c5c24d1d0c9271153a407a5dd8b34
murcafes94 <esaumuri94@gmail.com>
2026-08-27 14:04:55 -0500

    Bump NotCan to v0.7.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,8 +61,8 @@ android {
         applicationId = "com.notcan.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.7.2"
+        versionCode = 10
+        versionName = "0.7.3"
     }
 
     signingConfigs {
