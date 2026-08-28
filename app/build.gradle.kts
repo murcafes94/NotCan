3e38ab46bacb640adfc7203d1ed33a86e8209e9c
murcafes94 <esaumuri94@gmail.com>
2026-08-28 15:06:21 -0500

    v0.7.5: incrementa versión de la APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,8 +61,8 @@ android {
         applicationId = "com.notcan.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.7.4"
+        versionCode = 12
+        versionName = "0.7.5"
     }
 
     signingConfigs {
