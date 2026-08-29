abcf8a422cb2e027ea432d4370bed2490ad4ed13
murcafes94 <esaumuri94@gmail.com>
2026-08-29 12:13:30 -0500

    Sube versión a NotCan 0.7.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,8 @@ android {
         applicationId = "com.notcan.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.7.7"
+        versionCode = 15
+        versionName = "0.7.8"
     }
 
     signingConfigs {
